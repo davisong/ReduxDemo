8d47f57dd8ed004be84294f6cad42ea548291d23
davisong <davisong@umich.edu>
2021-11-07 12:46:36 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,72 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import friendsReducer from "./FriendsReducer";
+import HomeScreen from './HomeScreen';
+import FriendsScreen from './FriendsScreen';
+
+const Stack = createStackNavigator();
+const store = createStore(friendsReducer);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      possibleFriends: [
+        'Alice',
+        'Bob',
+        'Sammy',
+      ],
+      currentFriends: [],
+    }
+  }
+
+  addFriend = (index) => {
+    const {
+      currentFriends,
+      possibleFriends,
+    } = this.state
+
+    // Pull friend out of possibleFriends
+    const addedFriend = possibleFriends.splice(index, 1)
+
+    // And put friend in currentFriends
+    currentFriends.push(addedFriend)
+
+    // Finally, update the app state
+    this.setState({
+      currentFriends,
+      possibleFriends,
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Provider store={store}>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Friends" component={FriendsScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </Provider>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
